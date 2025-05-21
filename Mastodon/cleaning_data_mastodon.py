a6f8f11abf7ed8a3974eb1b9f9117d8b4a81a3d8
Anastasia <anastasia20040806@gmail.com>
2025-05-21 21:27:56 +0200

    Duplicated entries for multiple cves in one post

---
+++ Mastodon/cleaning_data_mastodon.py
@@ -0,0 +1,64 @@
+# Module for cleaning data from Mastodon posts
+
+#remove emojis, links, and hashtags from the text
+
+# return cleanes text in cvs
+
+import json
+import os
+import re
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+cve_posts = []
+cve_path = os.path.join(base_dir, 'cve_posts.json')
+if os.path.exists(cve_path):
+    with open(cve_path, "r", encoding="utf-8") as f:
+        cve_posts = json.load(f)
+
+def remove_emojis(text):
+    print("Removing emojis...")
+    emoji_pattern = re.compile(
+        "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+# Do not run!!!!
+# def duplicate_entry_if_many_cve(cve_list):
+#     # Check if there are multiple CVE entries in the list
+#     for cve in cve_list:
+#         if len(cve['cve']) > 1:
+#             print("Duplicating entries for multiple CVEs...")
+#             # Create a new entry for each CVE
+#             for multiple_cve in cve['cve']:
+#                 new_entry = cve.copy()
+#                 new_entry['cve'] = [multiple_cve]
+#                 cve_posts.append(new_entry)
+            
+#             cve_posts.remove(cve)
+                
+# for cve in cve_posts:
+#     cve['body'] = remove_emojis(cve['body'])
+
+# duplicate_entry_if_many_cve(cve_posts)
+
+def add_source(cve_list):
+    # Add the source to each CVE entry
+    print("Adding mastodon as a source to CVE entries...")
+    for cve in cve_list:
+        cve['source'] = "mastodon"
+
+add_source(cve_posts)
+with open(cve_path, "w", encoding="utf-8") as f:
+    print("Populating with CVE posts...")
+    json.dump(cve_posts, f, ensure_ascii=False, indent=2)
+
+
+# 
+            
+        
--- Mastodon/scraper.py
+++ Mastodon/scraper.py
@@ -7,14 +7,22 @@ from numpy import size
 import requests
 import json
 import os
+from cleaning_data_mastodon import remove_emojis
 
 url_timelines = 'https://mastodon.social/api/v1/timelines/public'
 url_for_hashtags = 'https://mastodon.social/api/v1/timelines/tag/cve'
 rate_limit = {"limit": "40"}
 cve_posts = []
 post_ids = []
-if os.path.exists("post_ids.json"):
-    with open("post_ids.json", "r", encoding="utf-8") as f:
+base_dir = os.path.dirname(os.path.abspath(__file__))
+cve_path = os.path.join(base_dir, 'cve_posts.json')
+ids_path = os.path.join(base_dir, 'post_ids.json')
+
+print(cve_path)
+print(ids_path)
+
+if os.path.exists(ids_path):
+    with open(ids_path, "r", encoding="utf-8") as f:
         post_ids = json.load(f)
 else:
     post_ids = []
@@ -26,7 +34,7 @@ def extract_cve(post):
     # Extract CVE IDs from the post content using regex
     cve_pattern = r'\b[Cc][Vv][Ee]-\d{4}-\d+\b'
     cve_ids = re.findall(cve_pattern, post)
-    return cve_ids.upper() if cve_ids else []
+    return cve_ids
 
 def parse_html_body(content):
     #parse the body of the post
@@ -35,49 +43,69 @@ def parse_html_body(content):
     # Get all visible text
     return soup.get_text(separator=" ")
 
-max_id = None
-for _ in range(100):  
+# max_id = None
+
+# print("Starting to scrape posts...")
+# print("Scraping posts by hashtags...")
+# for _ in range(100):  
+#     params = rate_limit.copy()
+#     if max_id:
+#         params["max_id"] = max_id
+#     r = requests.get(url_for_hashtags, params=params)
+#     posts = r.json()
+#     if not posts:
+#         break
+#     for t in posts:
+#         if contains_cve(t['content']) and t['id'] not in post_ids:
+#             post = {
+#                 "body": parse_html_body(t['content']),
+#                 "id": t['id'],
+#                 "created_at": t['created_at'],
+#                 "cve": extract_cve(t['content'])
+#             }
+#             if post["cve"]:
+#                 cve_posts.append(post)
+#                 post_ids.append(post["id"])
+#                 print("post found")
+
+#     max_id = posts[-1]['id']
+#     time.sleep(1)  # To avoid hitting rate limits
+            
+
+print("Scraping posts by timelines...")
+max_id_timelines = None
+
+for x in range(100):
     params = rate_limit.copy()
-    if max_id:
-        params["max_id"] = max_id
-    r = requests.get(url_for_hashtags, params=params)
+    if max_id_timelines:
+        params["max_id_timelines"] = max_id_timelines
+    r = requests.get(url_timelines, params=params)
     posts = r.json()
     if not posts:
         break
     for t in posts:
         if contains_cve(t['content']) and t['id'] not in post_ids:
-            post = {
-                "body": parse_html_body(t['content']),
-                "id": t['id'],
-                "created_at": t['created_at'],
-                "cve": extract_cve(t['content'])
-            }
-            if post["cve"]:
-                cve_posts.append(post)
-                post_ids.append(post["id"])
-    max_id = posts[-1]['id']
-    time.sleep(1)  # To avoid hitting rate limits
-            
-
-for x in range(300):
-    r = requests.get(url_timelines, params=rate_limit)
-    post = json.loads(r.text)
-    for t in post:
-        if contains_cve(t['content']) and t['id'] not in post_ids:
             post = {"body": parse_html_body(t['content']), "id": t['id'], "created_at": t['created_at'],  "cve": extract_cve(t['content'])}
             if post["cve"] != []:	
                 cve_posts.append(post)
                 post_ids.append(post["id"])
+                print("post found")
+    max_id_timelines = posts[-1]['id']
     time.sleep(1)  # To avoid hitting rate limits
 
-with open("cve_posts.json", "r", encoding="utf-8") as existing_file:
+with open(cve_path, "r", encoding="utf-8") as existing_file:
     existing_data = json.load(existing_file)
+    if cve_posts: 
+        for post in cve_posts:
+            remove_emojis(post["body"])
     cve_posts.extend(existing_data)
-with open("cve_posts.json", "w", encoding="utf-8") as f:
+with open(cve_path, "w", encoding="utf-8") as f:
+    print("Saving posts to cve_posts...")
     json.dump(cve_posts, f, ensure_ascii=False, indent=2)
 
 
-with open("post_ids.json", "w", encoding="utf-8") as f:
+with open(ids_path, "w", encoding="utf-8") as f:
+    print("Saving post ids to post_ids...")
     json.dump(post_ids, f, ensure_ascii=False, indent=2)
 
 
